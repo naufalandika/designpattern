8e6e06a6740fdae7c86be16421d19292ddbc55ab
Muhammad Naufal Andika Natsir Putra <muhammad.naufal@ruangguru.com>
2022-12-29 16:16:09 +0700

    create user-skill-course service

---
+++ trial/user-skill-course/entity.go
@@ -0,0 +1,5 @@
+package userskillcourse
+
+type GetQuestionsRequest struct{}
+
+type GetQuestionsResponse struct{}
---
+++ trial/user-skill-course/service.go
@@ -0,0 +1,17 @@
+package userskillcourse
+
+import "context"
+
+type Service interface {
+	GetQuestions(ctx context.Context, req *GetQuestionsRequest) (*GetQuestionsResponse, error)
+}
+
+func NewService() Service {
+	return &service{}
+}
+
+type service struct{}
+
+func (s *service) GetQuestions(ctx context.Context, req *GetQuestionsRequest) (*GetQuestionsResponse, error) {
+	return nil, nil
+}
