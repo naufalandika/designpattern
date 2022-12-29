086cd9ec838f6dbab5c392037ed3c56e763c5c07
Muhammad Naufal Andika Natsir Putra <muhammad.naufal@ruangguru.com>
2022-12-29 13:55:50 +0700

    create main

--- factory_method/main.go
+++ factory_method/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/naufalandika/designpattern/factory_method/service"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	go func() {
+		for {
+			t, _ := reader.ReadString('\n')
+			t = strings.TrimSuffix(t, "\n")
+			svc := service.New(t)
+
+			svc.GetQuestions()
+			fmt.Println()
+		}
+	}()
+
+	sigchnl := make(chan os.Signal, 1)
+	signal.Notify(sigchnl, os.Interrupt)
+	<-sigchnl
+}
