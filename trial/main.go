b154256c8f2d5b2b192d1dff4392b8a499721eee
Muhammad Naufal Andika Natsir Putra <muhammad.naufal@ruangguru.com>
2022-12-29 16:26:48 +0700

    improve main

--- trial/main.go
+++ trial/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/signal"
+	"regexp"
 	"strings"
+
+	userskillcourse "github.com/naufalandika/designpattern/trial/user-skill-course"
+)
+
+var (
+	uscSvc             = userskillcourse.NewService()
+	rgxGetQuestions, _ = regexp.Compile("getquestions/*")
 )
 
 func main() {
@@ -13,15 +23,27 @@ func main() {
 
 	go func() {
 		for {
-			t, _ := reader.ReadString('\n')
-			t = strings.TrimSuffix(t, "\n")
-			fmt.Println(t)
-
-			fmt.Println()
+			r, _ := reader.ReadString('\n')
+			r = strings.TrimSuffix(r, "\n")
+			resp, err := forwardReq(r)
+			if err != nil {
+				fmt.Println("error: ", err.Error())
+			} else {
+				fmt.Println("success: ", resp)
+			}
 		}
 	}()
 
 	sigchnl := make(chan os.Signal, 1)
 	signal.Notify(sigchnl, os.Interrupt)
 	<-sigchnl
+}
+
+func forwardReq(r string) (interface{}, error) {
+	switch {
+	case rgxGetQuestions.MatchString(r):
+		return uscSvc.GetQuestions(context.Background(), &userskillcourse.GetQuestionsRequest{})
+	default:
+		return nil, errors.New("unimplemented")
+	}
 }
