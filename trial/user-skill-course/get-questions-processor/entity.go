7fd5c10ae5abd8ef3bdf39cf33bf2031d5539e1e
Muhammad Naufal Andika Natsir Putra <muhammad.naufal@ruangguru.com>
2022-12-29 16:46:24 +0700

    implement GetGetQuestionsProcessor

--- trial/user-skill-course/get-questions-processor/entity.go
+++ trial/user-skill-course/get-questions-processor/entity.go
@@ -1,5 +1,18 @@
 package getquestionsprocessor
 
+type UserType int
+
+const (
+	NonPrakerjaUserType = iota
+	PrakerjaUserType
+)
+
+type CourseType int
+
+const (
+	OnlineCourseType = iota
+)
+
 type ValidateRequest struct{}
 
 type GetGetQuestionsProcessorRequest struct{}
--- trial/user-skill-course/get-questions-processor/processor.go
+++ trial/user-skill-course/get-questions-processor/processor.go
@@ -1,11 +1,30 @@
 package getquestionsprocessor
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type GetQuestionsProcessor interface {
 	Validate(context.Context, *ValidateRequest) error
 }
 
 func GetGetQuestionsProcessor(ctx context.Context, req *GetGetQuestionsProcessorRequest) GetQuestionsProcessor {
+	ut := getUserType(ctx)
+	ct := getCourseType(req)
+
+	switch {
+	case ut == PrakerjaUserType && ct == OnlineCourseType:
+		return &PrakerjaOnlineGetQuestionsProcessor{}
+	}
+
 	return nil
+}
+
+func getUserType(ctx context.Context) UserType {
+	return (UserType(time.Now().Nanosecond()) % 2)
+}
+
+func getCourseType(req *GetGetQuestionsProcessorRequest) CourseType {
+	return OnlineCourseType
 }
