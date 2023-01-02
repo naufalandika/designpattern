a7bc4eac6629f1c7fe8f15c3ea67a8c160639711
Muhammad Naufal Andika Natsir Putra <muhammad.naufal@ruangguru.com>
2023-01-02 16:17:04 +0700

    use already created value for GetProcessor

--- trial/user-skill-course/get-questions-processor/processor.go
+++ trial/user-skill-course/get-questions-processor/processor.go
@@ -4,6 +4,12 @@ import (
 	"context"
 )
 
+var (
+	prakerjaOnlineProcessor  *PrakerjaOnlineProcessor
+	prakerjaWebinarProcessor *PrakerjaWebinarProcessor
+	baseProcessor            *BaseProcessor
+)
+
 type Processor interface {
 	Validate(context.Context, *ValidateRequest) error
 }
@@ -11,10 +17,16 @@ type Processor interface {
 func GetProcessor(ctx context.Context, req *GetProcessorRequest) Processor {
 	switch {
 	case req.UserType == PrakerjaUserType && req.CourseType == OnlineCourseType:
-		return &PrakerjaOnlineProcessor{}
+		return prakerjaOnlineProcessor
 	case req.UserType == PrakerjaUserType && req.CourseType == WebinarCourseType:
-		return &PrakerjaWebinarProcessor{}
+		return prakerjaWebinarProcessor
 	default:
-		return &BaseProcessor{}
+		return baseProcessor
 	}
+}
+
+func init() {
+	prakerjaOnlineProcessor = &PrakerjaOnlineProcessor{}
+	prakerjaWebinarProcessor = &PrakerjaWebinarProcessor{}
+	baseProcessor = &BaseProcessor{}
 }
